784f29bcd1006c3791b21454bf7ff30fbfb9c1de
Burkay <bayirburkay@gmail.com>
2022-03-13 05:15:35 +0300

    theme style file create for dark mode

---
+++ src/assets/styles/Theme.js
@@ -0,0 +1,21 @@
+export const lightTheme = {
+  header: "#ffffff",
+  body: "#f7f7f8",
+  color: "#9147ff",
+  textColor: "#0e0e10",
+  soft: "#777777",
+  border: "#efeff1",
+  darkModeBg: "#dedede",
+  iconColor: "#0e0e10",
+};
+
+export const darkTheme = {
+  header: "#18181b",
+  body: "#0e0e10",
+  color: "#9147ff",
+  textColor: "#fcfaff",
+  soft: "#adadb8",
+  border: "#ffffff1a",
+  darkModeBg: "#9147ff",
+  iconColor: "#adadb8",
+};
