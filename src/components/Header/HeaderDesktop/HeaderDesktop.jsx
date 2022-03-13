8915b1fd1191b099b6678353747af93de5c608e1
Burkay <bayirburkay@gmail.com>
2022-03-13 05:19:39 +0300

    create header for desktop

---
+++ src/components/Header/HeaderDesktop/HeaderDesktop.jsx
@@ -0,0 +1,13 @@
+import { StyledHeaderDesktop } from "./HeaderDesktop.styled";
+
+import { BsTwitch } from "react-icons/bs";
+
+const HeaderDesktop = () => {
+  return (
+    <StyledHeaderDesktop>
+      <h1>Desktop</h1>
+    </StyledHeaderDesktop>
+  );
+};
+
+export default HeaderDesktop;
---
+++ src/components/Header/HeaderDesktop/HeaderDesktop.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledHeaderDesktop = styled.header`
+  display: none;
+
+  @media (min-width: 768px) {
+    display: block;
+  }
+`;
